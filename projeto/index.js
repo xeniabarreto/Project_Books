db30e59835ce20bd8fd94641a7d6e7d257b981f4
Xênia Barreto <xeniabarreto22@gmail.com>
2021-09-17 12:12:06 -0300

    Codando busca de livros por categoria

--- projeto/index.js
+++ projeto/index.js
@@ -9,3 +9,20 @@ console.log('\n---------------------------------------\n   Projeto - Sistema de 
 const booksTotal = books.length
 console.log(`\n Veja abaixo a lista de meus ${booksTotal} livros disponiveis: `)
 console.table(books)
+
+//ITEM 1 - buscar livros pela categoria 
+const seeBooksGenre = input.question('Deseja encontrar um livro por genero?(S/N): ').toLocaleUpperCase();
+
+if (seeBooksGenre === 'S'){ 
+    console.log('\nTemos seguintes generos:\n- AUTOAJUDA\n- BIOGRAFIA\n- PROFISSIONAL ACADEMICO\n- PSICOLOGIA\n- ROMANCE\n');
+    const whatsGenre = input.question('Qual genero deseja visualizar? ').toLocaleUpperCase();
+    console.log('\nEsses são os livros disponiveis no genero escolhido:');
+
+function genre(books){
+    return books.genre === whatsGenre
+}
+    const foundGenre = books.filter(genre);
+    console.table(foundGenre);
+}else{
+    console.log('\nNão tem problema! :)\nVamos manter em ordem alfabetica para sua melhor experiencia!\n');
+  }
