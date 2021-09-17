23e9519348c01557c99da675bc7f59651f515ee9
Xênia Barreto <xeniabarreto22@gmail.com>
2021-09-17 12:10:03 -0300

    Adicionando o database do Projeto

---
+++ projeto/database.js
@@ -0,0 +1,79 @@
+     
+      //cria o array de objetos estudantes 
+      const books = [
+        {
+          name: 'Rapido e devagar',
+          genre: 'PSICOLOGIA',
+          page: 608,
+          recommend: true,
+          read: false
+       
+        },
+        {
+          name: 'O que podemos aprender com os gansos',
+          genre: 'AUTOAJUDA',
+          page: 336,
+          recommend: true,
+          read: true
+        },
+        {
+          name: 'Não é coisa da sua cabeça',
+          genre: 'PSICOLOGIA',
+          page: 320,
+          recommend: true,
+          read: true
+        },
+        {
+          name: 'Inteligência do carisma',
+          genre: 'AUTOAJUDA',
+          page: 256,
+          recommend: true,
+          read: false
+        },
+        {
+          name: 'Os sete pilares da sabedoria',
+          genre: 'BIOGRAFIA',
+          page: 701,
+          recommend: false,
+          read: false
+        },
+        {
+          name: 'Java Script: O Guia Definitivo',
+          genre: 'PROFISSIONAL ACADEMICO',
+          page: 148,
+          recommend: true,
+          read: false
+    
+        },
+        {
+          name: 'Código limpo ',
+          genre: 'PROFISSIONAL ACADEMICO',
+          page: 425 ,
+          recommend: false,
+          read: false
+        },
+        {
+          name: 'Querido John',
+          genre: 'ROMANCE',
+          page: 288,
+          recommend: false,
+          read: true
+        },
+        {
+          name: 'A culpa e das estrelas',
+          genre: 'ROMANCE',
+          page: 255,
+          recommend: false,
+          read: false
+        },    
+        {
+          name: 'Marley & Eu',
+          genre: 'BIOGRAFIA',
+          page: 410,
+          recommend: false,
+          read: true
+        }
+    ]
+    
+    //exporta o array de objetos estudantes
+    module.exports = books
